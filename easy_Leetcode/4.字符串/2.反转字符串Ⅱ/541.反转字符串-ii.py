d2005cb833f2fcb562f02971c3cb4c6ee9a0a71c
zhongshao <zhongshao0122@gmail.com>
2024-12-11 23:05:59 +0800

    12.11test

---
+++ easy_Leetcode/4.字符串/2.反转字符串Ⅱ/541.反转字符串-ii.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=541 lang=python3
+#
+# [541] 反转字符串 II
+#
+
+# @lc code=start
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        
+        def reverse_substring(text):
+            left, right = 0, len(text) - 1
+            while left < right:
+                text[left], text[right] = text[right], text[left]
+                left += 1
+                right -= 1
+            return text
+        
+        res = list(s)
+
+        for cur in range(0, len(s), 2 * k):
+            res[cur: cur + k] = reverse_substring(res[cur: cur + k])
+
+        return ''.join(res)
+    
+# @lc code=end
+
