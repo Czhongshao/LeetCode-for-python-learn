4f5590bf22db558086a67e3c8175c5beba3d7457
zhongshao <zhongshao0122@gmail.com>
2025-07-23 12:07:07 +0800

    二叉树前序遍历

---
+++ easy_Leetcode/6.二叉树/3.二叉树的迭代遍历/144.二叉树的前序遍历.py
@@ -0,0 +1,38 @@
+#
+# @lc app=leetcode.cn id=144 lang=python3
+#
+# [144] 二叉树的前序遍历
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]: # type: ignore
+
+        ## 前序遍历
+        # 根节点为空则返回空列表
+        if not root:
+            return []
+
+        stack = [root]
+        result = []
+        while stack:
+            node = stack.pop()
+
+            # 中节点先处理
+            result.append(node.val)
+            # 右孩子先入栈
+            if node.right:
+                stack.append(node.right)
+            # 左孩子后入栈
+            if node.left:
+                stack.append(node.left)
+
+        return result
+# @lc code=end
+
